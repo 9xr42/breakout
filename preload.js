96d4ba90a5e9148f3898cf85720c2de1a2cf9f4c
Harshil Aggarwal <54381135+9xr42@users.noreply.github.com>
2024-07-23 00:14:48 -0500

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const elNodeVersion = document.getElementById('node-version');
+    elNodeVersion.innerText = process.versions['node'];
+
+    document.getElementById('chrome-version').innerText=process.versions['chrome'];
+    document.getElementById('electron-version').innerText=process.versions['electron'];
+
+});
